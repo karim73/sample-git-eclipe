923b36174ef6f6f437e7d2351bd2d0948aecf211
k.safraoui <k.safraoui@LFR00051>
2023-11-25 11:36:20 +0100

    add fields to User

--- src/org/karim/User.java
+++ src/org/karim/User.java
@@ -2,7 +2,39 @@ package org.karim;
 
 public class User {
 
+	private String firstName;
+	private String lastName;
+
+	
 	public User() {
-		// TODO Auto-generated constructor stub
 	}
+
+	public User(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
 }
